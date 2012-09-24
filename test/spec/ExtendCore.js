c9ac08d760dffc2463c82a14df297d785baa66ff
Bill Thompson <bill@billthompson.me>
2012-09-23 22:26:01 -0500

    first tests using Jasmine

---
+++ test/spec/ExtendCore.js
@@ -0,0 +1,29 @@
+describe("ExtendCore", function() {
+  var config, version = '1.0';
+
+  beforeEach(function() {
+    $(function() {
+      config = {
+        apiKey: '123456789', 
+        path: {
+          nonsecure: 'http://www.myorganization.com/site/', 
+          secure: 'https://secure2.convio.net/myorg/site/'
+        }
+      }
+
+      luminateExtend(config);
+    });
+  });
+
+  it("should have a version", function() {
+    expect(luminateExtend.library.version).toEqual(version);
+  });
+
+  it("can update global setting", function() {
+    var updatedAPIKey = '987654321';
+
+    luminateExtend.global.update('apiKey', updatedAPIKey); 
+
+    expect(luminateExtend.global.apiKey).toEqual(updatedAPIKey);
+  });
+});
